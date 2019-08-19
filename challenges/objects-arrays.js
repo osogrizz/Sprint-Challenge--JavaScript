10c0fe2c5432c2bb396d50f2edd169af1ab1088b
osogrizz <lhtor@gmail.com>
2019-08-18 18:53:23 -0700

    Adds Objects and Arrays solutions.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -62,7 +62,7 @@ console.log('Roar method:', tryannosaurus.roar() );
 
 
 // ==== Arrays ====
-
+console.log('Arrays')
 // Given an array of college graduates.  Complete the following requests using any array method you like
 
 const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
@@ -80,7 +80,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+graduates.map( grad => {
+  return universities.push(grad.university)
+})
+universities.sort();
+console.log('Request 1:', universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -89,16 +93,24 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
-console.log(contactInfo);
+graduates.map( grad => {
+  return contactInfo.push(`${grad.first_name}, ${grad.email}`)
+})
+console.log('Request 2:', contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
-console.log(uni);
+graduates.filter( university => {
+  if (university.university.toLocaleLowerCase().includes('uni')) {
+    return uni.push(university);
+  }
+})
+console.log('Request 3:', uni);
 
 
 // ==== ADVANCED Array Methods ====
-
+console.log('Advanced Arrays')
 // Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
 
 
@@ -119,7 +131,10 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
-console.log(animalNames);
+zooAnimals.forEach( animal => {
+  return animalNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`);
+})
+console.log('Request 1:', animalNames);
 
 /* Request 2: .map()    
 
@@ -128,7 +143,10 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 */
 
 const lowerCase = [];
-console.log(lowerCase); 
+zooAnimals.map( animal => {
+  return lowerCase.push(animal.animal_name.toLocaleLowerCase());
+})
+console.log('Request 2:', lowerCase); 
 
 /* Request 3: .filter() 
 
@@ -136,15 +154,25 @@ The zoos are concenred about animals with a lower population count. Find out whi
 
 */
 const lowerPopulation = [];
-console.log(lowerPopulation);
+zooAnimals.filter( animal => {
+  return animal.population < 5 ? lowerPopulation.push(animal) : null;
+})
+console.log('Request 3:', lowerPopulation);
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+let populationTotal = 0;
+let pops = zooAnimals.map( animal => {
+  return animal.population;
+})
+
+populationTotal = pops.reduce((a,b) => {
+  return a + b;
+})
+console.log('Request 4:', populationTotal);
 
 
 /* 
